553a544e844d9001c6c572d559740c91493cd221
hsonne <hauke.sonnenberg@gmx.de>
2018-05-26 03:04:12 +0200

    Start testing with (almost empty) test functions

---
+++ tests/testthat/test-testMain.R
@@ -0,0 +1,25 @@
+test_that("test_function() works", {
+
+  expect_error(.:::test_function())
+})
+
+test_that("loadArgs() works", {
+
+  expect_error(.:::loadArgs())
+})
+
+test_that("testColumnwiseIdentity() works", {
+
+  expect_error(.:::testColumnwiseIdentity())
+})
+
+test_that("printTestMessage() works", {
+
+  expect_error(.:::printTestMessage())
+})
+
+test_that("saveArgs() works", {
+
+  expect_error(.:::saveArgs())
+})
+
