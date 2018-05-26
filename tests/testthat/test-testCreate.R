225c5a4d62c9f9f350833dd47e4bfd49331d8a62
hsonne <hauke.sonnenberg@gmx.de>
2018-05-26 03:28:42 +0200

    Regenerate test files, now with package name (not ".")

--- tests/testthat/test-testCreate.R
+++ tests/testthat/test-testCreate.R
@@ -1,20 +1,20 @@
 test_that("create_test_files() works", {
 
-  expect_error(.:::create_test_files())
+  expect_error(kwb.test:::create_test_files())
 })
 
 test_that("get_code_to_test_source_file() works", {
 
-  expect_error(.:::get_code_to_test_source_file())
+  expect_error(kwb.test:::get_code_to_test_source_file())
 })
 
 test_that("get_function_assignments() works", {
 
-  expect_error(.:::get_function_assignments())
+  expect_error(kwb.test:::get_function_assignments())
 })
 
 test_that("get_test_for_function_assignment() works", {
 
-  expect_error(.:::get_test_for_function_assignment())
+  expect_error(kwb.test:::get_test_for_function_assignment())
 })
 
--- tests/testthat/test-testMain.R
+++ tests/testthat/test-testMain.R
@@ -1,25 +1,25 @@
 test_that("test_function() works", {
 
-  expect_error(.:::test_function())
+  expect_error(kwb.test:::test_function())
 })
 
 test_that("loadArgs() works", {
 
-  expect_error(.:::loadArgs())
+  expect_error(kwb.test:::loadArgs())
 })
 
 test_that("testColumnwiseIdentity() works", {
 
-  expect_error(.:::testColumnwiseIdentity())
+  expect_error(kwb.test:::testColumnwiseIdentity())
 })
 
 test_that("printTestMessage() works", {
 
-  expect_error(.:::printTestMessage())
+  expect_error(kwb.test:::printTestMessage())
 })
 
 test_that("saveArgs() works", {
 
-  expect_error(.:::saveArgs())
+  expect_error(kwb.test:::saveArgs())
 })
 
