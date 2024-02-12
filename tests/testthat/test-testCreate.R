d4524c904bd49c0114147708139eb9e8183d1a1c
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-02-12 12:47:22 +0100

    Remove test of get_function_assignments()
    The function has been moved to kwb.test

--- tests/testthat/test-testCreate.R
+++ tests/testthat/test-testCreate.R
@@ -13,27 +13,6 @@ test_that("get_test_codes_for_functions_in_file() works", {
   expect_error(kwb.test:::get_test_codes_for_functions_in_file())
 })
 
-test_that("get_function_assignments() works", {
-
-  f <- kwb.test:::get_function_assignments
-
-  expect_error(f())
-
-  file <- tempfile("test-", fileext = ".R")
-
-  writeLines(
-    text = c(
-      "id <- function(x) x",
-      "plus <- function(x, y) x + y"
-    ),
-    con = file
-  )
-
-  result <- f(file)
-
-  expect_identical(names(result), c("id", "plus"))
-})
-
 test_that("get_test_for_function_assignment() works", {
 
   expect_error(kwb.test:::get_test_for_function_assignment())
