26c181af328f7dea829124dcb6d57198a3779133
hsonne <hauke.sonnenberg@kompetenz-wasser.de>
2024-02-15 16:19:21 +0100

    Remove single_quoted() (never used)

--- R/utils.R
+++ R/utils.R
@@ -1,9 +1,3 @@
-# single_quoted ----------------------------------------------------------------
-single_quoted <- function(x)
-{
-  paste0("'", gsub("'", "\\\\'", x), "'")
-}
-
 # warn_if_file_exists ----------------------------------------------------------
 warn_if_file_exists <- function(test_file)
 {
